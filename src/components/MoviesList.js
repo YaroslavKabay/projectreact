0c0bce8fdf56ee6a283439845a69d131db248854
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-08-02 01:59:48 +0300

    fix

---
+++ src/components/MoviesList.js
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
+import {movieActions} from "../redux";
+
+export const MoviesList = () => {
+    const dispatch = useDispatch();
+    const {movies, isLoading, serverError} = useSelector(state => state.movieReducer);
+
+
+    useEffect(() => {
+            dispatch(movieActions.getMovies())
+    }, [])
+
+    console.log(movies);
+
+    return (<div>
+        Movies:
+        {movies.map(movie => <div key={movie.id}>
+            <h3>{movie.title}</h3>
+            {movie.vote_average}
+            <hr/>
+            {isLoading&& <h1>Loading</h1>}
+            {serverError&& <h1>{serverError}</h1>}
+
+        </div>)}
+    </div>)
+}
