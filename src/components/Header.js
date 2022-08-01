6fb98fab7a9d153db78a5578fba62e840c46f86f
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-08-02 01:07:23 +0300

    proect fetch 1 try

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h2>Header</h2>
+            <h2>Header lol</h2>
             <hr/>
         </div>
     );
